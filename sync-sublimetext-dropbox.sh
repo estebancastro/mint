99de32330ff7d20d30b9869e8a7bf07ea489ce6b
Esteban Castro <box.esteban@gmail.com>
2015-12-07 13:21:47 -0600

    package control > sublime sync

--- sync-sublimetext-dropbox.sh
+++ sync-sublimetext-dropbox.sh
@@ -7,4 +7,13 @@
 echo -e "Syncing Sublime Text 3 with Dropbox...\n\n\n"
 cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
 rm -r User
-ln -s ~/Dropbox/Sublime/User
+ln -s ~/Dropbox/Apps/Sublime/User
+
+
+
+
+
+echo -e "Installing Package Control...\n\n\n"
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/
+rm -r Package\ Control.sublime-package
+ln -s ~/Dropbox/Apps/Sublime/Installed\ Packages/Package\ Control.sublime-package
